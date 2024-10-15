cf5f78a8dd324a42f401a71a231e08a30a29a18e
Dávid Szomolai <david.szomolai@sostrv.sk>
2024-10-15 21:21:21 +0200

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("test")
+n_cisel = int(input())
+array_cisel = [0]*n_cisel
+vysledny_array = [0]*n_cisel
+
+for i in range(n_cisel):
+    array_cisel[i] = int(input())
+
+for i in range(n_cisel):
+    vysledny_array[i] = int(   (  (   bin(array_cisel[i])[2:]   ).zfill(32)   )[::-1], 2)
+    print(vysledny_array[i])
